7b4d0d65d03a7e010715e48fa633b64dfab14ce5
AustinChua <austinchuawx.22@ichat.sp.edu.sg>
2023-05-03 11:08:29 +0800

    done

--- bmi.py
+++ bmi.py
@@ -8,14 +8,14 @@ def calculate_bmi(height, weight):
     print("Your bmi is: " + bmi)
 
     if bmi <str(18.5):
-        num=-1
-        return(num)
+        num1=-1
+        return(num1)
     elif str(18.5)<=bmi<=str(25):
-        num=0
-        return(num)
+        num2=0
+        return(num2)
     elif bmi>str(25):
-        num=1
-        return(num)
+        num3=1
+        return(num3)
 
 
 calculate_bmi(height=1.73, weight=57.0)
